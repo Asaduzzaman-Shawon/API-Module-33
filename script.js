45b8a292644cc3c314c3acfdbed10efb656dfef4
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-20 15:21:00 +0600

    clg

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-function callF(){
-    fetch('https://jsonplaceholder.typicode.com/comments')
+// function callF(){
+//     fetch('https://jsonplaceholder.typicode.com/comments')
+//     .then(res => res.json())
+//     .then(data => displayData(data))
+// }
+
+// function displayData(data){
+//     for(const i of data){
+//         const nam = i.name
+//         const body = i.body
+//         const email = i.email
+//         const commentSection = document.getElementById('comment-section')
+//         const newComment = document.createElement('div')
+//         newComment.innerHTML = `
+//                 <h3>Name : ${nam} </h3>
+//                 <p>${body}</p>
+//                 <h5>${email}</h5>
+//         `
+//         commentSection.appendChild(newComment)
+//         newComment.classList.add('post')
+//     }
+// }
+
+// callF()
+console.log(1);
+console.log(2);
+console.log(3);
+function odd (){
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(data => displayData(data))
 }
 
 function displayData(data){
-
-    for(const i of data){
-        const nam = i.name
-        const body = i.body
-        const email = i.email
-        const commentSection = document.getElementById('comment-section')
-        const newComment = document.createElement('div')
-        
-        newComment.innerHTML = `
-                <h3>Name : ${nam} </h3>
-                <p>${body}</p>
-                <h5>${email}</h5>
-        `
-        commentSection.appendChild(newComment)
-        newComment.classList.add('post')
-    }
+    console.log(4)
 }
 
-callF()
+console.log(5);
+
+odd()
+
+console.log(5);
